3ac2381775120041a22654b3af993b493c967ca1
Jim Kang <jimkang@gmail.com>
2021-05-17 20:48:59 -0400

    Demo backing up the transport. Sadly, it behaves weirdly on Firefox, but is fine in Chrome.

--- vats/transport/transport.ts
+++ vats/transport/transport.ts
@@ -1,3 +1,33 @@
-console.log('hey');
+import { Sequence, FMSynth, Transport } from 'tone';
+
+var started = false;
+
+function start() {
+  if (started) {
+    return;
+  }
+
+  started = true;
+
+  var synthA = new FMSynth().toDestination();
+  new Sequence(
+    (time, note) => {
+      synthA.triggerAttackRelease(note, '4n', time);
+    },
+    ['C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2']
+  ).start(0);
+
+  Transport.start();
+}
+
+function backUp() {
+  const adjustedTicks = Transport.ticks - Transport.toTicks('4n');
+  if (adjustedTicks >= 0) {
+    Transport.ticks = adjustedTicks;
+  }
+}
+
+document.getElementById('start-button').addEventListener('click', start);
+document.getElementById('back-up-button').addEventListener('click', backUp);
 
 export default {};
